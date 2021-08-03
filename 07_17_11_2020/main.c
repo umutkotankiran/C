19e9e42b715c9a89e6bbf590c8b5d1109dc08a97
Umut KOTANKIRAN <79421649+umutkotankiran@users.noreply.github.com>
2021-08-03 14:18:19 +0300

    Add files via upload

--- 07_17_11_2020/main.c
+++ 07_17_11_2020/main.c
@@ -1,1 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
 
+int max(int a, int b)
+{
+	return a > b ? a : b;
+}
+
+
+int main()
+{
+	int x, y, z, t;
+	printf("4 sayi giriniz\n");
+	(void)scanf("%d%d%d%d", &x, &y, &z, &t);
+	
+	int ival = max(max(x, y),max(z,t));
+	printf("ival = %d\n",ival);
+}
