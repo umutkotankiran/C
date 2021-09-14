bba9a18a2d620bd4dca8b73c6a54f9de54c480c6
Umut KOTANKIRAN <79421649+umutkotankiran@users.noreply.github.com>
2021-09-15 00:03:33 +0300

    Add files via upload

---
+++ 45_06_04_2021/date.c
@@ -0,0 +1,326 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include "date.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define		YEARBASE	1900
+
+#define		isleap(y)		((y) % 4 == 0 && ((y) % 100 != 0 || (y) % 400  == 0))
+#define		mdays(y,m)		(daytabs[isleap(y)][m])
+#define		PRIVATE			static
+#define		PUBLIC			
+#define		RANDOM_YEAR_MAX		2021
+#define		RANDOM_YEAR_MIN		1940
+
+
+
+//artık gün hesabı buradan yazılacak.	
+PRIVATE const int daytabs[][13] = {
+	{0,31,28,31,30,31,30,31,31,30,31,30,31},
+	{0,31,29,31,30,31,30,31,31,30,31,30,31}
+};
+
+//-------------------------------------------------------------------------------------------
+//-------------------------------------------------------------------------------------------
+
+
+//hata işleme anlatılmadığı için bir fonksiyon yazılacak bunu yapan.
+//internal linkage da.başlık dosyasında yok.burada yaptıkki herkes tarafından kullanılabilsin.
+PRIVATE int is_valid_date(int d, int m, int y);
+PRIVATE Date* set(Date* p, int d, int m, int y);
+PRIVATE int totaldays(const Date*);
+
+// burası tarihin geçerliliğini sorguluyor.
+// artık yıl hesabıda dahil çünkü şubat gün farklılığı var.
+// bunuda hesaba katıyor.
+PRIVATE int is_valid_date(int d, int m, int y)
+{
+	return y >= YEARBASE && m > 0 && m <= 12 && d > 0 && d <= mdays(y, m);
+}
+
+//-------------------------------------------------------------------------------------------
+//-------------------------------------------------------------------------------------------
+
+PRIVATE Date* set(Date *p,int d, int m, int y)
+{
+	if (!is_valid_date(d, m, y))
+	{
+		fprintf(stderr, "gecersiz tarih\n");
+		exit(EXIT_FAILURE);
+	}
+
+	p->md = d;
+	p->mm = m;
+	p->my = y;
+
+	return p;
+}
+
+
+//-------------------------------------------------------------------------------------------
+//-------------------------------------------------------------------------------------------
+ 
+PRIVATE int totaldays(const Date *p) // belirtilen tarihten bu güne kaç gün olduğunu buluyoruz.
+{
+	int sum = get_year_day(p);
+	for (int i = YEARBASE; i < p->my; ++i)
+	{
+		sum += isleap(i) ? 366 : 365;
+	}
+	return sum;
+}
+
+
+//-------------------------------------------------------------------------------------------
+//-------------------------------------------------------------------------------------------
+
+
+// bu fonksiyon ÖNEMLİ !!!
+PRIVATE Date* to_date(Date* p, int totaldays)
+{
+	int y = YEARBASE;   
+	while (totaldays > (isleap(y) ? 366 : 365)) // totaldays in kaç yıl olduğuna baktı
+	{
+		totaldays -= (isleap(y) ? 366 : 365);
+		++y;
+	}
+	
+	int m = 1;
+	while (totaldays > (mdays(y,m))) // totaldays yıldan sonra kala kısımda kaç ay olduğuna baktı.
+	{
+		totaldays -= mdays(y, m);
+		++m;
+	}
+
+	int d = totaldays; // geri kalanda gün zaten
+	return set(p, d, m, y); // bunları p ye set etti ve return etti.
+
+}
+
+//-------------------------------------------------------------------------------------------
+//-------------------------------------------------------------------------------------------
+
+
+// c++ ta function definitionda bile parametrelere isim vermek mecburi değil.
+PUBLIC Date* set_date(Date *p, int d, int m, int y)
+{
+	return set(p, d, m, y); // setdate zaten aldığı Date *p yi geri döndürüyor. bunuda return ediyoruz
+}									// hata olsaydı hata yazacaktı zaten.Test edilecek.
+
+
+//-------------------------------------------------------------------------------------------
+//-------------------------------------------------------------------------------------------
+
+
+Date* set_date_today(Date* p)
+{
+	time_t timer;
+	time(&timer);
+	struct tm *tptr= localtime(&timer);
+
+	return set(p, tptr->tm_mday, tptr->tm_mon + 1, tptr->tm_year + 1900);
+
+}
+
+
+//-------------------------------------------------------------------------------------------
+//-------------------------------------------------------------------------------------------
+
+
+PUBLIC Date* set_date_from_str(Date*p, const char* pstr)
+{
+	int d = atoi(pstr);
+	int m = atoi(pstr + 3);
+	int y = atoi(pstr + 6);
+
+	return set(p, d, m, y);
+}
+
+
+//-------------------------------------------------------------------------------------------
+//-------------------------------------------------------------------------------------------
+
+
+PUBLIC Date* set_month(Date*p, int mon)
+{
+	return set(p, p->md, mon, p->my); // sadece ayı set ediyoruz. gün ve yıl zaten p içerisinde olan
+}										// gün veyıl olarak set edildi.yani aynı kaldı.
+							// doğrudan p->mon = mon denebilir ama geçerli mi geçersiz mi bunu
+							// sınamamış oluruz. bu yüzden set functiona gönderiliyor.
+
+//-------------------------------------------------------------------------------------------
+//-------------------------------------------------------------------------------------------
+
+
+
+PUBLIC Date* set_year(Date*p, int year)
+{
+	return set(p, p->md, p->mm, year);
+}
+
+
+//-------------------------------------------------------------------------------------------
+//-------------------------------------------------------------------------------------------
+
+
+PUBLIC Date* set_month_day(Date*p, int day)
+{
+	return set(p, day, p->mm, p->my);
+}
+
+
+//-------------------------------------------------------------------------------------------
+//-------------------------------------------------------------------------------------------
+
+
+//SIRADA GET FUNCTIONLAR VAR
+
+PUBLIC int get_year(const Date* p)
+{
+	return p->my;
+}
+
+//-------------------------------------------------------------------------------------------
+//-------------------------------------------------------------------------------------------
+
+PUBLIC int get_month(const Date* p)
+{
+	return p->mm;
+}
+
+//-------------------------------------------------------------------------------------------
+//-------------------------------------------------------------------------------------------
+
+PUBLIC int get_month_day(const Date* p)
+{
+	return p->md;
+}
+
+//-------------------------------------------------------------------------------------------
+//-------------------------------------------------------------------------------------------
+
+PUBLIC int get_year_day(const Date* p)
+{
+	//elimizde ayların gün sayısı var. günleri dolaşıp toplayacağız.
+	// o yıl içerisindeki kaçıncı günde olduğumu veriyor.
+	// mesela 06 04 2021 yılın 96. günü gibi.
+
+	int sum = p->md;
+	for (int i = 1; i < p->mm; ++i)
+	{
+		sum += mdays(p->my, i);
+	}
+
+	return sum;
+}
+
+//-------------------------------------------------------------------------------------------
+//-------------------------------------------------------------------------------------------
+
+PUBLIC int get_week_day(const Date* p)
+{
+	// Japon birinin algoritması ile haftanın günü hesaplanıyor.
+
+	const int t[] = { 0,3,2,5,0,3,5,1,4,6,2,4 };
+	int d = p->md;
+	int m = p->mm;
+	int y = p->my; // 0 pazar , 1 pazartesi...
+
+	if (m < 3)
+		--y;
+
+	return ((y + y / 4 - y / 100 + y / 400 + t[m - 1] + d) % 7);
+
+}
+
+//-------------------------------------------------------------------------------------------
+//-------------------------------------------------------------------------------------------
+
+PUBLIC void print_date(const Date* p)
+{
+	static const char* const pmons[] = {
+		"",
+		"Ocak",
+		"Subat",
+		"Mart",
+		"Nisan",
+		"Mayis",
+		"Haziran",
+		"Temmuz",
+		"Agustos",
+		"Eylul",
+		"Ekim",
+		"Kasim",
+		"Aralik",
+	};
+
+	static const char* const pdays[] = {
+		"Pazar",
+		"Pazartesi",
+		"Sali",
+		"Carsamba",
+		"Persembe",
+		"Cuma",
+		"Cumartesi",
+	};
+
+	printf("%02d %s %d %s\n", p->md, pmons[p->mm], p->my, pdays[get_week_day(p)]);
+
+}
+
+
+//-------------------------------------------------------------------------------------------
+//-------------------------------------------------------------------------------------------
+
+
+PUBLIC Date * set_date_random(Date *p)
+{
+	int y = rand() % (RANDOM_YEAR_MAX - RANDOM_YEAR_MIN + 1) + RANDOM_YEAR_MIN;
+	int m = rand() % 12 + 1;
+	int d = rand() % (mdays(y, m)) + 1;
+
+	return set(p, d, m, y);
+
+}
+
+//-------------------------------------------------------------------------------------------
+//-------------------------------------------------------------------------------------------
+
+
+PUBLIC Date *scan_date(Date* p)
+{
+	int d, m, y;
+	(void)scanf("%d%d%d", &d, &m, &y);
+	return set(p, d, m, y);
+}
+
+
+//-------------------------------------------------------------------------------------------
+//-------------------------------------------------------------------------------------------
+
+
+
+PUBLIC int cmp_date(const Date* p1, const Date* p2)
+{
+	if (p1->my != p2->my)
+		return p1->my - p2->my;
+
+	if (p1->mm != p2->mm)
+		return p1->mm - p2->mm;
+
+	return p1->md - p2->md;
+}
+
+
+PUBLIC int date_diff(const Date* p1, const Date* p2)
+{
+	return totaldays(p1) - totaldays(p2);
+}
+
+
+// psource tarihten n gun sonrasini pdest e atıyor.
+PUBLIC Date* ndays_date(Date* pdest, const Date* psource, int n)
+{
+	return to_date(pdest, totaldays(psource) + n);
+}
--- 45_06_04_2021/main.c
+++ 45_06_04_2021/main.c
@@ -1,1 +1,492 @@
+﻿
+
+/*
+
+	YAZARKEN KULLANDIĞIMIZ TESTLER.
+
+	Date mydate;
+	set_date(&mydate, 03, 04, 1990);
+	print_date(&mydate); 
+
+	set_date_today(&mydate);
+	print_date(&mydate); 
+
+	print_date(&mydate); 
+	set_date_from_str(&mydate, "12-10-2010");
+
+	set_date_from_str(&mydate, "07-04-2021");
+	printf("%d\n", get_week_day(&mydate)); // 0 pazar , 1 pazartesi, 2 Salı. 2 döndürüyor.
+
+	Date mydate;
+	randomize();
+
+	for (;;)
+	{
+		set_date_random(&mydate);
+		print_date(&mydate);
+		(void)getchar();
+	}
+
+	*/
+
+	/*
+
+	İKİNCİ TEST KODU
+
+	Date date;
+	printf("dogum tarihinizi girin\n");
+	scan_date(&date);
+	print_date(&date);
+
+	*/
+
+	/*
+
+	ÜÇÜNCU TEST KODU
+
+	Date date;
+	randomize();
+
+	for (int i = 0; i < 10; ++i)
+	{
+		set_date_random(&date);
+		print_date(&date);
+		printf("yil : %d\n", get_year(&date));
+		printf("ay : %d\n", get_month(&date));
+		printf("Ay gunu : %d\n", get_month_day(&date));
+		printf("hafta gunu : %d\n", get_week_day(&date));
+		system("pause");
+		system("cls");
+	}
+	*/
+
+	/*
+	
+	 ÜÇÜNCÜ TEST KODU
+	 
+	int dcmp(const void* vp1, const void* vp2)
+	{
+		return cmp_date((const Date*)vp1, (const Date*)vp2);
+	}
+
+	void set_date_array(Date* pa, size_t size)
+	{
+		while (size--)
+			set_date_random(pa++);
+	}
+
+	void print_date_array(const Date* p, size_t size)
+	{
+		while (size--)
+			print_date(p++);
+	}
+
+
+	int main()
+	{
+		int n;
+
+		printf("kac tarihi: ");
+		(void)scanf("%d", &n);
+
+		Date* pd = (Date*)malloc(n * sizeof(Date));
+		if (!pd)
+		{
+			printf("Yetersiz memory\n");
+			return 1;
+		}
+
+		randomize();
+		set_date_array(pd, n);
+	
+		printf("siralama basliyor\n");
+		clock_t start = clock();	
+		qsort(pd, n, sizeof *pd, &dcmp);
+		clock_t end = clock();	
+		printf("siralama bitti\n");
+
+		printf("islem suresi = %lf", (double)(end - start) / CLOCKS_PER_SEC);
+		clear_input_buffer();
+		(void)getchar(); // scanf in new line ını aldığı için durmuyor.buffer boşaltıyoruz yukarıda.
+		print_date_array(pd, n);
+		free(pd);	
+
+	}
+	
+	*/
+
+	/*
+		
+		DÖRDÜNCÜ TEST KODU
+
+		Date today, birth_Date;
+
+		printf("Dogum tarihinizi girin: \n");
+		scan_date(&birth_Date);
+		print_date(&birth_Date);
+
+		set_date_today(&today);
+		print_date(&today);
+		printf("bu rezil dunyada yasadigimiz %d. gun\n",date_diff(&today,&birth_Date));
+
+
+	*/
+
+/*
+
+	BEŞİNCİ TEST KODU
+
+	Date today, future_date;
+
+	set_date_today(&today);
+	printf("Bugun\n");
+	print_date(&today);
+
+	for (int i = 1; i < 1'000'000'001; i *= 10)
+	{
+		printf("bugunden %d gun sonraki tarih : ", i);
+		ndays_date(&future_date, &today, i);
+		print_date(&future_date);
+	}
+
+
+
+*/
+
+
+#pragma pack(1) // açıklanacak sonraki ders
+
+typedef struct {
+	int a, b;
+	double d;
+	char str[20];
+}Member;
+
+typedef struct {
+	int x, y;
+	Member m;
+}Nec;
+
+typedef struct {
+	int x, y;
+	Member *mp;
+}Nec1;
+
+/*
+	NOTLAR
+	------
+
+	BİRLEŞİK NESNELER / COMPOSITION
+
+	printf("sizeof(member) = %zu\n", sizeof(Member));
+	printf("sizeof(Nec) = %zu\n", sizeof(Nec));
+	//her nec içinde member var.
+
+	Nec nec;
+	nec.m.a = 10;
+	// karşımıza a.b.c.d gibi bir eleman çıkabilir.
+
+	Nec* np = &nec;
+	++np->m.a;
+
+	// yapının elemanı member değilde member türden pointer olsun.
+	Nec1 nec1;
+	//kodlar
+	nec1.mp->b = 20;
+	++nec1.mp->b;
+
+
+	// ----------------------------------------------------------
+
+	// birden fazla ok operatör de olabilir.
+	Nec1 anec;
+	Nec1* necptr = &anec;
+
+	necptr->mp->b = 30;
+
+	//-----------------------------------------
+
+	// hata olan durum var
+
+	//Yapının elemanı kendi türünden olamaz. Hem C hemde C++ ta böyle
+
+	/*struct Nec {
+		int a;
+		struct Nec nec; // nec uses undefined struct nec hatası verir
+	};
+
+	//------------------------------------------------------
+
+	//Ama pointer türden olabilir.
+	// çünkü pointer size bilgisi belli.
+
+	// bir yapının elemanı kendisinin pointerı türünden olabilir.
+	// ama kendisi türünden olamaz.	
+
+struct Nec {
+	int a;
+	struct Nec* pnec; // böyle yapılara self referencial structure deniyor. 
+};						// veriyapılarında sıksık kullanılıyor.
+
+//----------------------------------------------------
+
+// bir struct elemanı struct ise bunu initialize edebiliyoruz
+
+typedef struct {
+	int a, b;
+	double d;
+	char str[20];
+}Member;
+
+typedef struct {
+	int x, y;
+	Member m;
+	int z;
+}Nec;
+
+//main içinden aşağıdaki gibi devam edilir.
+Nec nec = { 1,2,{1,2,3.54,"alican"},2 };
+
+// yada designated initializer 
+Nec nec1 = { .x = 2, .y = 20, .m = {1,2,3.4,"alihan"},.z = 30 };
+
+
+
+*/
+
+/*
+
+	Sonraki örnek
+
+	Employee emp;
+	randomize();
+	for (;;)
+	{
+		set_employee_random(&emp);
+		print_employee(&emp);
+		_getch();
+	}
+
+	*/
+
+	/*
+	
+	Sonraki Örnek
+	-------------
+
+	int n;
+	printf("Kac calisan\n");
+	(void)scanf("%d", &n);
+
+	Employee* pd;
+
+	if (!(pd = (Employee*)malloc(n * sizeof(*pd)))) // *pd yerine Employee de yazılabilir.
+	{
+		fprintf(stderr, "Bellek yetersiz\n");
+		return 1;
+	}
+
+	for (int i = 0; i < n; ++i)
+	{
+		set_employee_random(pd + i);
+	}
+	
+
+	// qsort(pd, n, sizeof(*pd), &cmp_employee_by_no_); deniyorum o yüzden yorum yaptım
+	//qsort(pd, n, sizeof(*pd), &cmp_employee_by_name_); 
+	qsort(pd, n, sizeof(*pd), &cmp_employee_by_town_); // diğer kalanlarda yapılabilir.
+	(void)getchar();
+	for (int i = 0; i < n; ++i)
+	{
+		print_employee(pd + i);
+	}
+
+	free(pd);
+	
+
+	*/
+
+/*
+
+	SONRAKİ
+
+	int n;
+
+	Employee* pd;
+
+	// burası önemli
+	const CMPFUNC fa[] = { &cmp_employee_by_no_,&cmp_employee_by_name_,&cmp_employee_by_surname_,
+	&cmp_employee_by_town_,&cmp_employee_by_date_ };
+
+	printf("Kac calisan\n");
+	(void)scanf("%d", &n);
+
+	if (!(pd = (Employee*)malloc(n * sizeof(*pd)))) // *pd yerine Employee de yazılabilir.
+	{
+		fprintf(stderr, "Bellek yetersiz\n");
+		return 1;
+	}
+
+	for (int i = 0; i < n; ++i)
+	{
+		set_employee_random(pd + i);
+	}
+
+	for (size_t i = 0; i < asize(fa); ++i) // tüm sıralama kriterlerine göre burada
+	{										// sıralama yapıyor. ad soyad şehir...
+		qsort(pd, n, sizeof(*pd), fa[i]);
+		for (int k = 0; k < n; ++k)
+		{
+			print_employee(pd + k);
+		}
+		(void)_getch();
+		system("cls");
+	}
+
+	(void)getchar();
+
+	free(pd);
+
+
+*/
+
+
+/*
+	SONRAKI GÜZEL ÖRNEK
+
+	void print_menu(void)
+{
+	printf(" 1 - numaraya gore sirala\n"
+		" 2 - isme gore sirala\n"
+		" 3 - soyisme gore sirala\n"
+		" 4 - sehire gore sirala\n"
+		" 5 - dogum tarihe gore sirala\n\n");
+
+	printf("secenegi girin :\n");
+}
+
+void  print_employee_array(const Employee* p,size_t size)
+{
+	while (size--)
+		print_employee(p++);
+}
+
+int main()
+{
+	
+	int n;
+	Employee* pd;
+
+	// burası önemli
+	const CMPFUNC fa[] = { NULL,&cmp_employee_by_no_,&cmp_employee_by_name_,&cmp_employee_by_surname_,
+	&cmp_employee_by_town_,&cmp_employee_by_date_ };
+
+	printf("Kac calisan\n");
+	(void)scanf("%d", &n);
+
+	if (!(pd = (Employee*)malloc(n * sizeof(*pd)))) // *pd yerine Employee de yazılabilir.
+	{
+		fprintf(stderr, "Bellek yetersiz\n");
+		return 1;
+	}
+
+	for (int i = 0; i < n; ++i)
+	{
+		set_employee_random(pd + i);
+	}
+
+	for (;;)
+	{
+		print_menu();
+		int option;
+		(void)scanf("%d", &option);
+		qsort(pd,n,sizeof(Employee),fa[option]);	
+		print_employee_array(pd, n);
+		_getch();
+		system("cls");
+	}
+
+	free(pd);
+
+
+}
+
+*/
+
+
+
+
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include "nutility.h"
+#include "date.h"
+#include <time.h>
+#include "Employee.h"
+//yukarıda pragma pack1 de tanımı
+#include <conio.h>
+
+
+/*
+	burada cmp employee functionları void ile yazacağız.
+	qsort türden bağımsız karşılaştırma fonksiyonları ile implemente ediliyor.
+	var olanların sonuna _ koyup void ile yazacağız
+
+*/
+
+
+int cmp_employee_by_no_(const void*vp1, const void*vp2)
+{
+	return cmp_employee_by_no((const Employee*)vp1, (const Employee*)vp2);
+}
+
+int cmp_employee_by_name_(const void* vp1, const void* vp2)
+{
+	return cmp_employee_by_name((const Employee*)vp1, (const Employee*)vp2);
+}
+
+int cmp_employee_by_surname_(const void* vp1, const void* vp2)
+{
+	return cmp_employee_by_surname((const Employee*)vp1, (const Employee*)vp2);
+}
+
+int cmp_employee_by_town_(const void* vp1, const void* vp2)
+{
+	return cmp_employee_by_town((const Employee*)vp1, (const Employee*)vp2);
+}
+
+int cmp_employee_by_date_(const void* vp1, const void* vp2)
+{
+	return cmp_employee_by_date((const Employee*)vp1, (const Employee*)vp2);
+}
+
+
+typedef int (*CMPFUNC)(const void*, const void*);
+
+
+// NOT: C STANDARTLARINA GÖRE FUNCTION ADRESLERİ ARASINDA TÜR DÖNÜŞÜMÜ YAPMAK
+// UNDEFINED BEHAVIOR AMA DEREYİCİLER BUNA İZİN VERİYOR.DOĞRU KOD ÜRETİYOR.
+// ÖZELLİKLE CONST VOİD* PARAMETRELERİ SÖZKONUSU OLDUĞUNDA.
+
+// BU DURUMDA CMP FUNCTIONLARIN SONUNA _ KOYDUĞUMUZ VOID TÜRÜNDEN FUNCTIONLARI YAZMAYA 
+// GEREK KALMIYOR ARTIK
+// AŞAĞIDA ÖRNEĞİ VAR
+
+/*
+
+const CMPFUNC fa[] = { NULL,
+				(CMPFUNC)&cmp_employee_by_no,
+				(CMPFUNC)&cmp_employee_by_name,
+				(CMPFUNC)&cmp_employee_by_surname,
+				(CMPFUNC)&cmp_employee_by_town,
+				(CMPFUNC)&cmp_employee_by_date };
+
+//ÖNCEKİ ÖRNEKTE &cmp_employee_by_no YERİNE &cmp_employee_by_no_ İSİMLİ FONKSİYON
+// YAZILMIŞTI VOID TÜRDEN PARAMETRELİ OLARAK.AMA BU CASTING SAYESİNDE YENİ FUNCTION YAZMAYA
+// GEREK YOK.
+// STANDARTLARA GÖRE UNDEFINED BEHAVIOR FAKAT COMPILERLAR DOĞRU KODA İZİN VERİYOR.
+// BİZ WRAPPER YAZDIK. BİRÇOĞU BU ŞEKİLDE CASTİNG YAPIYOR.
+
+*/
 
